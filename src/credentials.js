2f57f046b77640bb4455771d66eae496c15b212b
Mohansingh Omprakash <mohan.singh1385664@gmail.com>
2021-07-18 14:25:57 +0530

    Delete credentials.js

--- src/credentials.js
+++
@@ -1,5 +0,0 @@
-export const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-export const API_KEY = "AIzaSyBIDdIj92DnPXN-ki-4dhp9NJgqStyHhwE";
-export const CLIENT_ID = "1048980049109-ma71dtvqkkbmlcufrdv1nevpdrbubpul.apps.googleusercontent.com"
-export const SCOPES =
-  "https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl"
